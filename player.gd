5893d99d505e138ac7a8db77b5647580886ac9f9
lemon-shark-25 <196343170+lemon-shark-25@users.noreply.github.com>
2026-08-07 16:13:41 +0200

    Added inertia and sprinting

--- player.gd
+++ player.gd
@@ -1,12 +1,22 @@
 extends CharacterBody3D
 
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
-const SENSITIVITY = 0.003
-
 @onready var head = $Head
 @onready var camera = $Head/Camera3D
+
+## CONSTANTS
+const WALK_SPEED = 5.0
+const SPRINT_SPEED = 15.0
+const SENSITIVITY = 0.003
+const MAX_STAMINA = 100
+const REGEN_TIME = 1.5
+
+## VARIABLES
+var speed = WALK_SPEED
+var stamina := MAX_STAMINA
+var can_regen := false
+
+## FUNCTIONS
 
 func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
@@ -22,19 +32,21 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
-#	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-#		velocity.y = JUMP_VELOCITY
+	# Handle spint
+	if Input.is_action_pressed("sprint"):
+		speed = SPRINT_SPEED
+	else:
+		speed = WALK_SPEED
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("left", "right", "up", "down")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var direction = (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		velocity.x = direction.x * speed
+		velocity.z = direction.z * speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		velocity.x = lerp(velocity.x, direction.x * speed, delta * 7.0)
+		velocity.z = lerp(velocity.z, direction.z * speed, delta * 7.0)
 
 	move_and_slide()
