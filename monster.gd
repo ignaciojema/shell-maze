589796e8c3dd9ff08b01aad5152c04292f0f44ba
lemon-shark-25 <196343170+lemon-shark-25@users.noreply.github.com>
2026-08-08 23:28:04 +0200

    Added non-finished monster AI

--- monster.gd
+++ monster.gd
@@ -4,6 +4,29 @@ extends CharacterBody3D
 
 var speed = 3.5
 
+var angle_cone_of_vision := deg_to_rad(30.0)
+var max_view_distance := 800.0
+var angle_between_rays := deg_to_rad(5.0)
+
+func generate_raycasts() -> void:
+	var ray_count := angle_cone_of_vision / angle_between_rays
+
+	for index in ray_count:
+		var ray := RayCast2D.new()
+		var angle := angle_between_rays * (index - ray_count / 2.0)
+		ray.cast_to = Vector2.UP. rotated(angle) * max_view_distance
+		add_child(ray)
+		ray.enabled = true
+
+#func _physics_process (delta: float):
+#	var target = null
+#	for ray in get_children():
+#		if ray.is_colliding() and ray.get_collider().is_in_group("player"):
+#			target = ray.get_collider()
+#		break
+#
+#	var does_see_player := target != null
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	# Add the gravity.
