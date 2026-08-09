213d699bc4d3be148ffdc932e7d57d441094e3a8
Juan Huiza <162555162+ElSrMapache@users.noreply.github.com>
2026-08-09 14:58:19 +0200

    Revert "Merge branch 'MenuPausa'"
    This reverts commit 36f0c444bf8cd551b87ca03922c567ca0796419b, reversing
    changes made to 3efd1025cfa43384efece4c662d506ea35a94d0f.

--- score.gd
+++ score.gd
@@ -4,4 +4,4 @@ extends Label
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	self.text = str(player.score)
+	self.text = str("Score: ", player.score)
