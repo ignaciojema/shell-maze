6cdb2d908c9363787c49d5c0ecc6a5e68a560754
XDZR8 <162550802+XDZR8@users.noreply.github.com>
2026-08-09 19:08:18 +0200

    Shell grab sound
    works

--- shell.gd
+++ shell.gd
@@ -1,6 +1,7 @@
 extends Area3D
 
 ## CONSTANTS
+@onready var audio = $AudioStreamPlayer3D
 
 ## VARIABLES
 
@@ -9,4 +10,8 @@ extends Area3D
 func _on_body_entered(body: Node3D) -> void:
 	if body.is_in_group("player"):
 		body.inc_score()
+		visible = false
+		monitoring = false
+		audio.play()
+		await audio.finished
 		queue_free()
