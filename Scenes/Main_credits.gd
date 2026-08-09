79fc3c97be7ed2ce55e0af4b95bf0feea7b35384
Juan Huiza <162555162+ElSrMapache@users.noreply.github.com>
2026-08-09 04:36:49 +0200

    Main tittle & credits

---
+++ Scenes/Main_credits.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+var button_type = null
+
+func _on_back_pressed() -> void:
+	button_type = "Back"
+	$ColorRect.show()
+	$ColorRect/Fade_Timer.start()
+	$ColorRect/AnimationPlayer.play("fade_in")
+
+
+func _on_fade_timer_timeout() -> void:
+	if button_type == "Back" :
+		get_tree().change_scene_to_file("res://Scenes/MainMenu.tscn")
+
+func _ready():
+	$FadeOut/AnimationPlayer.play("fade_out")
+	await get_tree().create_timer(1.0).timeout
+	$FadeOut.hide()
