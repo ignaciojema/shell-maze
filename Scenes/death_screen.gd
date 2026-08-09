6020da005e8e362d4dacf962ec54ba59639ef7c2
lemon-shark-25 <196343170+lemon-shark-25@users.noreply.github.com>
2026-08-09 22:04:25 +0200

    Added death sound

---
+++ Scenes/death_screen.gd
@@ -0,0 +1,23 @@
+extends Node2D
+
+@onready var death_audio := $DeathAudio
+
+var button_type = null
+
+func _on_main_menu_button_pressed() -> void:
+	button_type = "Back"
+	$ColorRect.show()
+	$ColorRect/Fade_Timer.start()
+	$ColorRect/AnimationPlayer.play("fade_in")
+
+
+func _on_fade_timer_timeout() -> void:
+	if button_type == "Back" :
+		get_tree().change_scene_to_file("res://Scenes/MainMenu.tscn")
+
+func _ready():
+	death_audio.play()
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)	# Hide mouse
+	$FadeOut/AnimationPlayer.play("fade_out")
+	await get_tree().create_timer(1.0).timeout
+	$FadeOut.hide()
